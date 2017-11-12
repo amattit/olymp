87a3f5d3dd50f6896d114841929a87c26984101e
Mikhail Seregin <_silo@mail.ru>
2017-11-12 15:25:09 +0300

    Initial Commit

---
+++ olymp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  olymp
+//
+//  Created by Mikhail Seregin on 12.11.2017.
+//  Copyright © 2017 Mikhail Seregin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
