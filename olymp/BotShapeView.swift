100a714a31da1028b20b6862565b5f0fd1f2ff0c
Mikhail Seregin <_silo@mail.ru>
2017-11-12 16:44:41 +0300

    firstCommit

---
+++ olymp/BotShapeView.swift
@@ -0,0 +1,23 @@
+//
+//  BotShapeView.swift
+//  olymp
+//
+//  Created by Mikhail Seregin on 12.11.2017.
+//  Copyright © 2017 Mikhail Seregin. All rights reserved.
+//
+
+import UIKit
+
+class BotShapeView: ShapeView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+
+}
--- olymp/ViewController.swift
+++ olymp/ViewController.swift
@@ -10,8 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var shapeView: ShapeView!
+    
+    var botShapeView:[BotShapeView] = []
+    
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("start complete")
+        
+        for _ in 0...10 {
+            
+            let location = CGPoint(x: Double(arc4random_uniform(300)), y: Double(arc4random_uniform(600)))
+            
+            let bot = BotShapeView(frame: CGRect(x: location.x, y: location.y, width: 30, height: 30))
+            bot.layer.cornerRadius = bot.radius
+            bot.backgroundColor = UIColor.red
+            
+            botShapeView.append(bot)
+            
+        }
+        
+        for bot in botShapeView {
+            
+            
+            self.view.addSubview(bot)
+            
+        }
+        
+        
+        
+//        createPanGestureRecognizer(targetView: shapeView)
+//        shapeView.backgroundColor = UIColor.yellow
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +58,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
